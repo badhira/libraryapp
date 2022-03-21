aa715cd854a0de5cb858a958c2bf6bec439f52d6
badhira <badhirabasheer7@gmail.com>
2022-03-21 21:17:12 +0530

    second  commit

--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/library');
+mongoose.connect('mongodb+srv://userone:userone@ictk.lc1et.mongodb.net/libraryapp?retryWrites=true&w=majority');
 const Schema = mongoose.Schema;
 
 

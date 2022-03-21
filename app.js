4b077566fd840b1907e0389e9d2280045bf1920c
badhira <badhirabasheer7@gmail.com>
2022-03-21 14:04:38 +0530

    second  commit

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
-const express=require('express');
+const express = require('express'); 
+const path = require ('path'); 
+const cors = require('cors');
+const bodyParser=require('body-parser')
 
-const app  = express();
-// setting view object
-app.set('view engine','ejs');
-app.set('views','./src/views');
+
+
+const loginRouter = require('./src/routes/loginroute');
+const signupRouter = require('./src/routes/signuproute');
+const homeRouter = require('./src/routes/homeroute');
+const booksRouter = require('./src/routes/booksroute');
+const authorsRouter = require('./src/routes/authorsroute');
+
+const app = new express; 
+
+
+app.set('views','./src/views'); 
+app.set('view engine','ejs'); 
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname , '/public'))); 
+app.use(cors());
+
+app.use('/login',loginRouter); 
+app.use('/signup',signupRouter); 
+app.use('/home',homeRouter); 
+app.use('/books',booksRouter); 
+app.use('/authors',authorsRouter); 
+
+
 
 app.get('/',function(req,res){
-    res.render("index",{books});
+
+    res.render('index',{});
+    
 });
 
-app.listen(3000);
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT,()=>{
+    console.log(`Our Library app is running on port ${PORT}`);
+});
+
+
